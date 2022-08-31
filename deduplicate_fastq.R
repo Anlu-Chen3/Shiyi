dc93bb59b85b15a3bcf07bc07ca1c896c37f0233
yinshiyi <yinshiyi2020@gmail.com>
2022-08-31 23:22:53 +0000

    add microseq function

--- deduplicate_fastq.R
+++ deduplicate_fastq.R
@@ -4,7 +4,9 @@
 
 library(data.table)
 library(dplyr)
-
+library("microseq")
+# install.packages("microseq")
+# https://rdrr.io/cran/microseq/man/readFastq.html
 args = commandArgs(trailingOnly=TRUE)
 
 f<-read.table(args[1])
@@ -19,4 +21,4 @@ for (i in 1:length(f3$V1)) {
   tmp<-rbind(f3$V1[i],f3$V2[i],f3$V3[i],f3$V4[i])
   df<-rbind(df,tmp)
 }
-write.table(df,"df.txt",quote = F,col.names = F,row.names =F )
+write.table(df,"df.fq",quote = F,col.names = F,row.names =F )
