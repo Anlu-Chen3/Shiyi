6855b9e049272a2dad29e2008bd1c8f4278b9aa2
Chen <chenax23@USFLA-0MUMD6W.abbvienet.com>
2022-08-31 15:36:56 -0700

    shiyi

---
+++ deduplicate_fastq.R
@@ -0,0 +1,21 @@
+
+setwd("~/Documents/shiyi")
+
+
+library(data.table)
+library(dplyr)
+
+
+f<-read.table("A9161.fastq.out.fq")
+
+f2<-as.data.frame(matrix(f$V1, ncol = 4, byrow = TRUE))
+
+f3<-f2%>%
+  distinct(V1,.keep_all = T)
+
+df<-NULL
+for (i in 1:length(f3$V1)) {
+  tmp<-rbind(f3$V1[i],f3$V2[i],f3$V3[i],f3$V4[i])
+  df<-rbind(df,tmp)
+}
+write.table(df,"df.txt",quote = F,col.names = F,row.names = )
