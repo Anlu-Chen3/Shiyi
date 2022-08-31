5bc81a4478650a4528971fb5698558966e878c84
Chen <chenax23@USFLA-0MUMD6W.abbvienet.com>
2022-08-31 16:43:40 -0700

    anlu edit

--- deduplicate_fastq.R
+++ deduplicate_fastq.R
@@ -8,7 +8,19 @@ library(dplyr)
 
 f<-read.table("A9161.fastq.out.fq")
 
-f2<-as.data.frame(matrix(f$V1, ncol = 4, byrow = TRUE))
+f2<-NULL
+tmp<-NULL
+for (i in 1:(length(f$V1)/4)) {
+  a<-f$V1[4*(i-1)+1]
+  b<-f$V1[4*(i-1)+2]
+  c<-f$V1[4*(i-1)+3]
+  d<-f$V1[4*(i-1)+4]
+  tmp<-as.data.frame(cbind(a,b,c,d))
+  f2<-rbind(f2,tmp)
+}
+
+
+# f2<-as.data.frame(matrix(f$V1, ncol = 4, byrow = TRUE))
 
 f3<-f2%>%
   distinct(V1,.keep_all = T)
@@ -19,3 +31,6 @@ for (i in 1:length(f3$V1)) {
   df<-rbind(df,tmp)
 }
 write.table(df,"df.txt",quote = F,col.names = F,row.names = )
+
+nchar(df)
+nchar(f)
