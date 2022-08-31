86cac9daba0fd169d0061964bebe877e2fa10c4f
yinshiyi <yinshiyi2020@gmail.com>
2022-08-31 22:50:17 +0000

    add arg[1] support

--- deduplicate_fastq.R
+++ deduplicate_fastq.R
@@ -1,12 +1,13 @@
 
-setwd("~/Documents/shiyi")
+#setwd("~/Documents/shiyi")
 
 
 library(data.table)
 library(dplyr)
 
+args = commandArgs(trailingOnly=TRUE)
 
-f<-read.table("A9161.fastq.out.fq")
+f<-read.table(args[1])
 
 f2<-as.data.frame(matrix(f$V1, ncol = 4, byrow = TRUE))
 
@@ -18,4 +19,4 @@ for (i in 1:length(f3$V1)) {
   tmp<-rbind(f3$V1[i],f3$V2[i],f3$V3[i],f3$V4[i])
   df<-rbind(df,tmp)
 }
-write.table(df,"df.txt",quote = F,col.names = F,row.names = )
+write.table(df,"df.txt",quote = F,col.names = F,row.names =F )
